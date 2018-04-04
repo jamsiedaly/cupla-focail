bc40d67bcce1a1d5e96603fa9e693325f79e7086
jamsiedaly <1415278@studentmail.ul.ie>
2018-04-04 22:52:10 +0100

    Fancy interafce

--- rap_battler.py
+++ rap_battler.py
@@ -1,28 +1,49 @@
 from __future__ import print_function
-import tensorflow as tf
-
-import phonetics
-import spellyzer
 
 import argparse
 import os
+import time
+
+import tensorflow as tf
+from fabulous import image
+from fabulous import text
+from fabulous.color import bold, red, blue, green
+from model import Model
 from six.moves import cPickle
 
-from model import Model
 
-from six import text_type
+def openscreen():
 
-import pyttsx
+    clear_screen = chr(27) + "[2J"
+    biggie = image.Image('./img/Biggie.jpg', width=75)
+    biggie_in_lines = str(biggie).splitlines()
+    tabbed_biggie = ''
+    for line in biggie_in_lines:
+        line = '\t\t\t\t\t\t' + line
+        tabbed_biggie = tabbed_biggie + line + '\n'
+
+    message = bold('''\t\t\t\t\t\t\t\t\tRap Battler by James Daly.
+        \t\t\t\t\t\tWhen  you type a line hit enter to move to the next line.
+        \t\t\t\t\t\tYou and the AI will take turns writing two lines at a time.''')
+
+    lay_bars = text.Text("Lay some bars...", color="#ff8800", shadow=True, skew=0)
+    gap = '\n\n'
+
+
+    print(clear_screen)
+    print(tabbed_biggie)
+    print(message)
+    print(lay_bars)
+    print(gap)
 
 def main():
-    engine = pyttsx.init()
-    engine.setProperty('voice', 'en-scottish')  # changes the voi
+
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--sample', type=int, default=1,
                         help='0 to use max at each timestep, 1 to sample at '
                              'each timestep, 2 to sample on spaces')
-    parser.add_argument('--turns', type=int, default=5,
+    parser.add_argument('--turns', type=int, default=10,
                         help='Number of turns the game will play out')
 
     args = parser.parse_args()
@@ -32,17 +53,47 @@ def main():
         saved_args = cPickle.load(f)
         model = Model(saved_args, training=False)
 
-    print('Lay some bars... \n')
-    for i in range(args.turns):
-        input = get_input()
-        raw_text = battle(input, args.sample, model)
-        output = format_text(raw_text)
-        for i in range(2):
-            print(output[i])
-            engine.say(output[i])
-        engine.runAndWait()
 
-model = None
+    you = text.Text('You:', color='#0022ff', shadow=False, fsize=14)
+    split_lines = '\n\n' + bold(green('\t\t\t\t____________________________________________________________________________________________________________________________________________')) + '\n\n'
+    indent = '\t\t\t\t\t\t\t'
+
+    having_fun = True
+
+    while(having_fun):
+        openscreen()
+        for i in range(args.turns):
+            print(you)
+            input = get_input()
+            print(split_lines)
+            raw_text = battle(input, args.sample, model)
+            output = format_text(raw_text)
+            print(text.Text('AI:', color='#ff2200', shadow=False, fsize=14))
+            for i in range(2):
+                print((indent + output[i]))
+                time.sleep(0.5)
+            print(split_lines)
+            time.sleep(0.5)
+        answered = False
+        answer = raw_input(text.Text("Play again? yes or no", color="ffffff", shadow=False, skew=6))
+        answer = answer.lower()
+        if answer == 'yes' or answer == 'y':
+            having_fun = True
+            answered = True
+        elif answer == 'no' or answer == 'n':
+            having_fun = False
+            answered = True
+        else:
+            while not answered:
+                answer = raw_input('Please answer yes or no!\t')
+                if answer == 'yes' or answer == 'y':
+                    having_fun = True
+                    answered = True
+                elif answer == 'no'  or answer == 'n':
+                    having_fun = False
+                    answered = True
+
+
 
 def battle(prime, sample, model):
     char_pool_size = 2000
@@ -59,8 +110,8 @@ def battle(prime, sample, model):
             return generated_text
 
 def get_input():
-    text1 = raw_input("\n")
-    text2 = raw_input("")
+    text1 = raw_input('\t\t\t\t\t\t\t')
+    text2 = raw_input('\t\t\t\t\t\t\t')
     text = text1 + text2
     return text
 
